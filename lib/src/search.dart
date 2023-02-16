08ce400defd8325aed59221fbc3e5a7f2af8b8aa
anonymous <anonymous>
2023-02-16 17:02:08 +0800

    update flutter uikit to 1.6.2

--- lib/src/search.dart
+++ lib/src/search.dart
@@ -3,8 +3,6 @@ import 'package:tencent_cloud_chat_uikit/tencent_cloud_chat_uikit.dart';
 
 import 'package:timuikit/src/provider/theme.dart';
 import 'package:provider/provider.dart';
-import 'package:timuikit/src/provider/user_guide_provider.dart';
-import 'package:timuikit/utils/user_guide.dart';
 
 class Search extends StatelessWidget {
   const Search(
@@ -29,54 +27,49 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isConversation = (conversation != null);
-    judgeGuide('search', context);
     final theme = Provider.of<DefaultThemeData>(context).theme;
-    final guideModel = Provider.of<UserGuideProvider>(context);
-    return IndexedStack(index: guideModel.guideName != "" ? 0 : 1, children: [
-      UserGuide(guideName: guideModel.guideName),
-      Scaffold(
-        appBar: AppBar(
-          iconTheme: const IconThemeData(
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        elevation: 0,
+        backgroundColor: theme.primaryColor,
+        title: Text(
+          isConversation
+              ? (conversation?.showName ??
+                  conversation?.conversationID ??
+                  TIM_t("相关聊天记录"))
+              : TIM_t("全局搜索"),
+          style: const TextStyle(
             color: Colors.white,
-          ),
-          elevation: 0,
-          backgroundColor: theme.primaryColor,
-          title: Text(
-            isConversation
-                ? (conversation?.showName ??
-                    conversation?.conversationID ??
-                    TIM_t("相关聊天记录"))
-                : TIM_t("全局搜索"),
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 17,
-            ),
+            fontSize: 17,
           ),
         ),
-        body: isConversation
-            ? TIMUIKitSearchMsgDetail(
-                isAutoFocus: isAutoFocus,
-                currentConversation: conversation!,
-                onTapConversation: onTapConversation,
-                keyword: initKeyword ?? "",
-              )
-            : TIMUIKitSearch(
-                isAutoFocus: isAutoFocus,
-                onEnterSearchInConversation:
-                    (V2TimConversation conversation, String keyword) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Search(
-                          onTapConversation: onTapConversation,
-                          conversation: conversation,
-                          initKeyword: keyword,
-                        ),
-                      ));
-                },
-                onTapConversation: onTapConversation,
-              ),
-      )
-    ]);
+      ),
+      body: isConversation
+          ? TIMUIKitSearchMsgDetail(
+              isAutoFocus: isAutoFocus,
+              currentConversation: conversation!,
+              onTapConversation: onTapConversation,
+              keyword: initKeyword ?? "",
+            )
+          : TIMUIKitSearch(
+              isAutoFocus: isAutoFocus,
+              onEnterSearchInConversation:
+                  (V2TimConversation conversation, String keyword) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Search(
+                        onTapConversation: onTapConversation,
+                        conversation: conversation,
+                        initKeyword: keyword,
+                      ),
+                    ));
+              },
+              onTapConversation: onTapConversation,
+            ),
+    );
   }
 }
