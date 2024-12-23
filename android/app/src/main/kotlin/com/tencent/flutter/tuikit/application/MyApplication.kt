145e57ca990ba270c03b0ca7576160d845ec573b
vinsonswang <vinsonswang@tencent.com>
2024-12-23 17:49:46 +0800

    Optimize offline push configuration

--- android/app/src/main/kotlin/com/tencent/flutter/tuikit/application/MyApplication.kt
+++ android/app/src/main/kotlin/com/tencent/flutter/tuikit/application/MyApplication.kt
@@ -2,14 +2,8 @@ package com.tencent.flutter.tuikit.application
 
 import com.tencent.chat.flutter.push.tencent_cloud_chat_push.application.TencentCloudChatPushApplication;
 
-class TIMApplication : TencentCloudChatPushApplication () {
+class MyApplication : TencentCloudChatPushApplication () {
     override fun onCreate() {
         super.onCreate()
     }
 }
-
-//class TIMApplication {
-//    override fun onCreate() {
-//        super.onCreate()
-//    }
-//}
