c1535b2394f3abec2503b3bff6511646c00a7d57
vinsonswang <vinsonswang@tencent.com>
2025-03-31 10:17:17 +0800

    init branch v2

--- android/app/src/main/kotlin/com/tencent/flutter/tuikit/application/MyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.tencent.flutter.tuikit.application
-
-import com.tencent.chat.flutter.push.tencent_cloud_chat_push.application.TencentCloudChatPushApplication;
-
-class MyApplication : TencentCloudChatPushApplication () {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
